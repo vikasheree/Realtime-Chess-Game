aa1f5a426aa776160c04531a291314ee62062d80
Vikas Mishra <vikasmishrag1470@gmail.com>
2026-03-17 11:51:44 +0530

    restore working backend version

--- chess.vikas/src/main/java/com/chess/vikas/repository/UserRepository.java
+++ chess.vikas/src/main/java/com/chess/vikas/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.chess.vikas.repository;
-
 import com.chess.vikas.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
