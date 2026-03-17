e2ad9dcf42d26336eea9eb9f3a079f130d27eebe
Vikas Mishra <vikasmishrag1470@gmail.com>
2026-03-17 11:07:36 +0530

    Disable database completely for deployment

--- chess.vikas/src/main/java/com/chess/vikas/MainApplication.java
+++ chess.vikas/src/main/java/com/chess/vikas/MainApplication.java
@@ -3,7 +3,10 @@ package com.chess.vikas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 public class MainApplication {
 
 	public static void main(String[] args) {
